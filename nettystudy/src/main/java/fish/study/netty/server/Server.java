0401a2532fdefa742c0e353f9df76722ddba635c
yuxz <yxzjerryfish@hotmail.com>
2019-12-02 03:31:31 -0800

    init

---
+++ nettystudy/src/main/java/fish/study/netty/server/Server.java
@@ -0,0 +1,23 @@
+package fish.study.netty.server;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class Server {
+    public static void main(String[] args) {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        NioEventLoopGroup w = new NioEventLoopGroup();
+        NioEventLoopGroup r = new NioEventLoopGroup();
+
+        bootstrap.group(w,r).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<NioSocketChannel>() {
+            protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+
+            }
+        }).bind(6546);
+    }
+}
