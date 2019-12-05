4eaa8cea71ad2ec4416f57d4c24aaa8da2782456
yuxz <yxzjerryfish@hotmail.com>
2019-12-05 16:12:31 +0800

    client

--- nettystudy/src/main/java/fish/study/netty/client/Client.java
+++ nettystudy/src/main/java/fish/study/netty/client/Client.java
@@ -1,9 +1,19 @@
 package fish.study.netty.client;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 public class Client {
     public static void main(String[] args) {
@@ -13,10 +23,23 @@ public class Client {
 
         bootstrap.group(w)
                 .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<NioSocketChannel>() {
-                    @Override
-                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                .handler(new ChannelInitializer<NioServerSocketChannel>() {
 
+                    @Override
+                    protected void initChannel(NioServerSocketChannel nioServerSocketChannel) throws Exception {
+                        nioServerSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                System.out.println(LocalDateTime.now()+ ": 客户端写出数据");
+                                byte[] bytes = "你好!".getBytes(StandardCharsets.UTF_8);
+
+                                ByteBuf buffer = ctx.alloc().buffer();
+
+                                buffer.writeBytes(bytes);
+
+                                ctx.channel().writeAndFlush(buffer);
+                            }
+                        });
                     }
                 });
 
--- nettystudy/src/main/java/fish/study/netty/server/Server.java
+++ nettystudy/src/main/java/fish/study/netty/server/Server.java
@@ -2,10 +2,16 @@ package fish.study.netty.server;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 public class Server {
     public static void main(String[] args) {
@@ -17,7 +23,14 @@ public class Server {
         bootstrap.group(w,r).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<NioSocketChannel>() {
             @Override
             protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                nioSocketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+                    @Override
+                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                        ByteBuf byteBuf = (ByteBuf) msg;
 
+                        System.out.println(LocalDateTime.now() + ": 服务端读到数据 -> " + byteBuf.toString(StandardCharsets.UTF_8));
+                    }
+                });
             }
         }).bind(6546);
     }
