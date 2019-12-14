2a4a8d6cf800a993b0cb9f1c466d02cc1d4c9bfd
Fish Paradise <yxzjerryfish@hotmail.com>
2019-12-14 21:27:38 +0800

    客户端登录成功

--- nettystudy/src/main/java/fish/study/netty/server/handler/ServerHandler.java
+++ nettystudy/src/main/java/fish/study/netty/server/handler/ServerHandler.java
@@ -1,6 +1,39 @@
 package fish.study.netty.server.handler;
 
+import com.alibaba.fastjson.JSON;
+import fish.study.netty.model.LoginRequestPacket;
+import fish.study.netty.model.LoginResponsePacket;
+import fish.study.netty.model.Packet;
+import fish.study.netty.packet.PacketCode;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
+import java.time.LocalDateTime;
+
 public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println(JSON.toJSON(msg));
+
+        Packet packet = PacketCode.INSTANCE.decode(byteBuf);
+
+        if (packet instanceof LoginRequestPacket) {
+            LoginRequestPacket loginRequestPacket = (LoginRequestPacket) packet;
+
+            if (valid(loginRequestPacket)) {
+                System.out.println(LocalDateTime.now() + ": 客户端登录成功");
+            } else {
+                System.out.println(LocalDateTime.now()  + ": 客户端登录失败");
+            }
+        }
+    }
+
+    private Boolean valid(LoginRequestPacket loginRequestPacket){
+        System.out.println("客户端请求ID ： "+loginRequestPacket.getUserId());
+        System.out.println("客户端请求密码：" +loginRequestPacket.getPassword());
+        return true;
+    }
 }
