33c3369884f19637683cd2b9485554fdbb1811e5
Fish Paradise <yxzjerryfish@hotmail.com>
2020-01-12 18:58:11 +0800

    一个demo

--- anotherTest/src/main/java/fish/study/Test.java
+++ anotherTest/src/main/java/fish/study/Test.java
@@ -7,12 +7,17 @@ import fish.study.model.B;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.time.LocalDateTime;
+
 public class Test {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigB.class);
-        A a = applicationContext.getBean(A.class);
-        B b = applicationContext.getBean(B.class);
-
-        System.out.println(a.getA() + b.getB());
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigB.class);
+//        A a = applicationContext.getBean(A.class);
+//        B b = applicationContext.getBean(B.class);
+//
+//        System.out.println(a.getA() + b.getB());
+        LocalDateTime l = LocalDateTime.of(2019,4,16,0,0);
+        LocalDateTime m = l.plusDays(168);
+        System.out.println(m.toLocalDate());
     }
 }
